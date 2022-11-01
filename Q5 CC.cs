110f0208236c89325c77e7d8ec1d59317e960fd5
chukshith <53993293+chukshith@users.noreply.github.com>
2022-11-01 11:32:39 +0530

    Add files via upload

---
+++ Q5 CC.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pay	
+{
+    public class Program       
+    {
+        public static void Main(string[] args)     
+        {
+            Console.WriteLine("Enter the number of hours worked");
+            int hoursWorked = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"The calculated Net pay is ${CalculateNetPay(hoursWorked)}");
+        }
+
+        public static double GetGrossPay(int hoursWorked)
+        {
+            double grossPay;
+
+            if (hoursWorked < 56)
+                grossPay = 56 * 80;
+            else
+            {
+                int extraHours = hoursWorked - 56;
+                grossPay = (extraHours * 1.5 * 80) + (56 * 80);
+            }
+
+            return grossPay;
+        }
+
+        public static double CalculateTax(double grossPay)
+        {
+            double deductableTax;
+
+            deductableTax = grossPay * 0.02;
+
+            return deductableTax;
+        }
+
+        public static double CalculateNetPay(int hoursWorked)
+        {
+            double netPay, grossPay, deductableTax;
+
+            grossPay = GetGrossPay(hoursWorked);
+            deductableTax = CalculateTax(grossPay);
+            netPay = grossPay - deductableTax;
+
+            return netPay;
+        }
+    }
+}
